47cd360f676a81cdc735fc8e0749f9c05f8369f4
xetiro <rubengeraldes@gmail.com>
2020-08-28 13:24:25 +0900

    Implemented the requestCameraPermission so it can access the camera with user consent.

--- app/src/main/java/com/example/mytvapp/MainActivity.java
+++ app/src/main/java/com/example/mytvapp/MainActivity.java
@@ -1,19 +1,76 @@
 package com.example.mytvapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 public class MainActivity extends Activity {
+    public static String TAG = "MainActivityDebug";
+    private static int ACCESS_CAMERA_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setResolutionSpinner();
+        requestCameraPermission();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: " + permissions.length);
+        if(requestCode == ACCESS_CAMERA_REQUEST_CODE ) {
+            if(grantResults[0] == PERMISSION_GRANTED) {
+                Log.d(TAG, "Camera Permission Granted");
+            } else {
+                Log.d(TAG, "Camera Permission Denied");
+            }
+        }
+    }
+
+    private void requestCameraPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PERMISSION_GRANTED) {
+            Log.d(TAG, "requestCameraPermission: Permission already granted !!!");
+            return;
+        }
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+            Log.d(TAG, "requestCameraPermission: Permission needed. Explain the user why you need to use the camera");
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.request_camera_title))
+                    .setMessage(getString(R.string.request_camera_message))
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, ACCESS_CAMERA_REQUEST_CODE);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            Log.d(TAG, "requestCameraPermission: Permission needed but no explanation needed. Show the permission dialog prompt.");
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, ACCESS_CAMERA_REQUEST_CODE);
+        }
+    }
+
+    private void setResolutionSpinner() {
         Spinner spinner = (Spinner) findViewById(R.id.cameraResolutionSpinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
